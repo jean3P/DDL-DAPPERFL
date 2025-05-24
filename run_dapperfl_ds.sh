b185899a1cf8f481c8a4de9eb867643bf3f59d14
Erazo Botero Jairo David (je18n046) <je18n046@submit04.ubelix.unibe.ch>
2025-05-24 13:03:38 +0200

    DS experiments

--- run_dapperfl_ds.sh
+++ run_dapperfl_ds.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name=run_dapperfl     
-#SBATCH --output=run/run_dapperfl_AD.out
-#SBATCH --error=run/run_dapperfl_AD.err
-#SBATCH --time=02:00:00             
+#SBATCH --job-name=run_dapperfl_ds
+#SBATCH --output=run/run_dapperfl_CD_PR_7_75.out
+#SBATCH --error=run/run_dapperfl_CD_PR_7_75.err
+#SBATCH --time=02:00:00
 #SBATCH --partition=gpu          
 #SBATCH --ntasks=1                   
 #SBATCH --cpus-per-task=4            
 #SBATCH --mem=64G                     
-#SBATCH --gpus=h100:1
+#SBATCH --gpus=rtx4090:1
 
 module load CUDA/11.8.0
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate ddl-env
-export WANDB_API_KEY=268b0fb16203164678d2e0cddc9f291c4285c791
-python ./src/main.py \
-         --model dapperfl \
-         --dataset fl_officecaltech \
-         --backbone resnet18 \
-         --communication_epoch 100 \
-         --local_epoch 5 \
-         --parti_num 10 \
-         --pr_strategy AD \
-         --alpha 0.9 \
-         --alpha_min 0.1 \
-         --epsilon 0.2 \
-         --reg_coeff 0.01 \
-         --wandb 0 \
-         --device_id 0
+export WANDB_API_KEY=467ef7609483ffc67883540f9aff415f436814a9
+python ./src/continuous_domain_shift_experiment.py \
+        --model dapperfl \
+        --dataset fl_officecaltech \
+        --backbone resnet18 \
+        --parti_num 10 \
+        --communication_epoch 100 \
+        --local_epoch 5 \
+        --shift_frequency 7 \
+        --shift_ratio 0.75 \
+        --pr_strategy progressive \
+        --alpha 0.9 \
+        --alpha_min 0.1 \
+        --epsilon 0.2  \
+        --experiment_name CD_PR_7_75
