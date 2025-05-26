8073718719ef57ef3d04d9f85d4d81f9894a1a3b
Erazo Botero Jairo David (je18n046) <je18n046@submit04.ubelix.unibe.ch>
2025-05-26 09:48:26 +0200

    Varying number of clients

--- run_dapperfl_100.sh
+++ run_dapperfl_100.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=run_dapperfl     
-#SBATCH --output=run/run_dapperfl_TPR_0.1.out
-#SBATCH --error=run/run_dapperfl_TPR_0.1.err
-#SBATCH --time=02:00:00             
+#SBATCH --output=run/run_dapperfl_0.2_20.out
+#SBATCH --error=run/run_dapperfl_0.2_20.err
+#SBATCH --time=04:00:00
 #SBATCH --partition=gpu          
-#SBATCH --ntasks=1                   
-#SBATCH --cpus-per-task=4            
-#SBATCH --mem=64G                     
-#SBATCH --gpus=h100:1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --gpus=rtx4090:1
 
 module load CUDA/11.8.0
 source $(conda info --base)/etc/profile.d/conda.sh
@@ -19,13 +19,11 @@ python ./src/main.py \
          --backbone resnet18 \
          --communication_epoch 100 \
          --local_epoch 5 \
-         --parti_num 10 \
+         --parti_num 20 \
          --pr_strategy 0.2 \
          --alpha 0.9 \
          --alpha_min 0.1 \
          --epsilon 0.2 \
          --reg_coeff 0.01 \
          --wandb 0 \
-         --device_id 0 \
-         --noise_var 0.1 \
-         --noise_clients 8 9
+         --device_id 0
